df51de789100a810ce3cdfb5d7967c0905d87aa1
tmajdali <t.majdaly@gmail.com>
2017-05-14 18:34:44 +0300

    01
    01

---
+++ smile.py
@@ -0,0 +1,54 @@
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+
+cascPath = "haarcascade_smile.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.225,
+        minNeighbors=85,
+        minSize=(80, 80)
+    )
+
+    # Draw a rectangle around the faces
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, 'U R Smiling!', (x, y-10), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
